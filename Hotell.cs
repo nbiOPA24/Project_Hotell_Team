eac8fefcb600fa89281c6def1dd3480f0012af10
Tobias Lampinen <tobias.lampinen@student.nbi-handelsakademin.se>
2024-10-23 19:39:51 +0200

    Initial commit

---
+++ Hotell.cs
@@ -0,0 +1,17 @@
+﻿//To do list project hotellet.
+//Grovt är detta vad jag tänker ska ingå i programmet, det kommer säkerligen revideras men för att ha en utgångspunkt. 
+
+//To do list för Hotellhanteringssystemet
+
+//Generella uppgifter (gäller hela systemet):
+//Rum rumstyp, kapacitet, pris, status ledig, bokad, etc.
+//Bokningar kund, rum, datum, status
+//Kunder (namn, kontakt info)
+//Personal (namn, roll)
+//Reviews (rum, kund, betyg, kommentar)
+
+//Skapa användarroller:
+//Definiera kund och personalroller med olika rättigheter (ex. kunden får bara boka, personal får hantera rum och bokningar).Tänker få in lite private och protected )
+//Bygga autentisering och login-system:
+//Kunder och personal ska kunna logga in med olika användarroller.
+//Designa en lättnavigerad användargränssnitt där kunder och personal enkelt kan använda systemet.
